ae83d0eae227df0f4bfc10ab0a49325f54bf0ca5
Cheryl Chen <yhcchen@outlook.com>
2019-01-02 11:13:04 -0500

    pep8 clean up

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, render_template, url_for
-import requests, json, csv, numpy as np, pandas as pd, sys
+from flask import Flask, request, render_template
+import requests, json, numpy as np, pandas as pd
 import pymongo
 from src.api_data import download_data
 from src.data_cleanup import cleanup_data
@@ -23,12 +23,12 @@ else:
     pet_data = download_data()
     pet_df = pd.DataFrame(pet_data)
     pet_df = cleanup_data(pet_df)
-    save_cols = ['pet_id', 'age', 'breeds', 'description', 'media', 'sex', \
-                'size', 'status']
+    save_cols = ['pet_id', 'age', 'breeds', 'description', 'media', 'sex',
+                 'size', 'status']
     for i in range(len(pet_df)):
         # convert single row to json format to save to database
         data_json = json.loads(pet_df[save_cols].loc[i].to_json())
-        
+
         # set examination to avoid save duplicate data to database
         key = {'pet_id': int(pet_df.loc[i, 'pet_id'])}
         my_coll.update_one(key, {'$setOnInsert': data_json}, upsert=True)
@@ -48,8 +48,9 @@ def mainproject():
     random_ids = random_df['pet_id'].tolist()
     image_list = random_df['media'].tolist()
     selection = dict()
-    
-    return render_template('capstone.html', random_pets=zip(random_ids, image_list))
+
+    return render_template('capstone.html', random_pets=zip(random_ids,
+                                                            image_list))
 
 
 @app.route('/recommresult', methods=['GET', 'POST'])
@@ -59,27 +60,27 @@ def recomm_result():
         selected_ids = set(selection.values())
         user_feature_df = make_user_feature(df, selected_ids)
 
-        recomm_mat = np.dot(user_feature_df.iloc[:, 1:], pet_feature_df.iloc[:, 1:].T)
-         # return 10 highest number ids, names, breeds
-        max_idx_10 =  recomm_mat.argsort(axis=1)[:, :-11:-1]
+        recomm_mat = np.dot(user_feature_df.iloc[:, 1:],
+                            pet_feature_df.iloc[:, 1:].T)
+        # return 10 highest number ids, names, breeds
+        max_idx_10 = recomm_mat.argsort(axis=1)[:, :-11:-1]
         recomm_id = list()
-        
+
         for idx in max_idx_10[0]:
             recomm_id.append(pet_feature_df.loc[idx, 'pet_id'].tolist())
-        
 
         recomm_names = list()
         recomm_breeds = list()
         recomm_images = list()
         for id in recomm_id:
-            recomm_names.append(df[df['pet_id']==id]['name'].values[0])
-            recomm_breeds.append(df[df['pet_id']==id]['breeds'].values[0])
-            recomm_images.append(df[df['pet_id']==id]['media'].values[0])
+            recomm_names.append(df[df['pet_id'] == id]['name'].values[0])
+            recomm_breeds.append(df[df['pet_id'] == id]['breeds'].values[0])
+            recomm_images.append(df[df['pet_id'] == id]['media'].values[0])
 
-    return render_template('recom_result.html',top10= zip(recomm_id,\
-                            recomm_names, recomm_breeds, recomm_images))
-
-
+    return render_template('recom_result.html', top10=zip(recomm_id,
+                                                          recomm_names,
+                                                          recomm_breeds,
+                                                          recomm_images))
 
 # -------------main------------------
 if __name__ == "__main__":
@@ -87,8 +88,5 @@ if __name__ == "__main__":
     # load database to dataframe
     df = pd.DataFrame(list(my_coll.find()))
     pet_feature_df = make_pet_feature(df)
-
-    
-
 
     app.run(host='0.0.0.0', port=8080, debug=True)
--- src/data_cleanup.py
+++ src/data_cleanup.py
@@ -1,8 +1,9 @@
 import numpy as np, pandas as pd, re
 
+
 def sep_breeds(x):
     '''
-    separate all breeds mentioned in breed type for 
+    separate all breeds mentioned in breed type for
     breed type in dataframe
     '''
     if len(x['breed']) == 1:
@@ -25,20 +26,24 @@ def extract_image_url(x):
 
     # exclude issue with no image included at all by using
     # x.get('photos',{'photo':[{'$t':'n/a'}]}).get('photo')
-    num_url = len(x.get('photos',{'photo':[{'$t':'n/a'}]}).get('photo'))
+    num_url = len(x.get('photos', {'photo': [{'$t': 'n/a'}]}).get('photo'))
 
     url_set = set()
     for i in range(num_url):
-        url_set.add(''.join(re.findall(url_pattern, x.get('photos',{'photo':[{'$t':'n/a'}]}).get('photo')[i].get('$t'))))
+        url_set.add(''.join(re.findall(url_pattern, x.get('photos',
+                                                          {'photo':
+                                                              [{'$t': 'n/a'}]})
+                            .get('photo')[i].get('$t'))))
     return sorted(url_set)
+
 
 def cleanup_data(df):
     '''
     cleanup datafram store raw format of pet_data
     '''
     # set columns to extract dictionary data
-    map_col = ['animal', 'age', 'description','id', 'lastUpdate','mix', \
-            'name', 'sex', 'shelterId', 'shelterPetId', 'size', 'status']
+    map_col = ['animal', 'age', 'description', 'id', 'lastUpdate', 'mix',
+               'name', 'sex', 'shelterId', 'shelterPetId', 'size', 'status']
 
     for col in map_col:
         df[col] = df[col].apply(lambda x: x.get('$t'))
@@ -48,7 +53,7 @@ def cleanup_data(df):
 
     # set pet_id with integer type
     df['pet_id'] = df['id'].astype('int')
-    df.drop(columns = ['id'], inplace = True)
+    df.drop(columns=['id'], inplace=True)
 
     # extract all images urls
     df['media'] = df['media'].apply(extract_image_url)
